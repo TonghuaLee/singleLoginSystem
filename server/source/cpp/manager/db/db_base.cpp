cae9b439066916b046306fc2c7755286ff839e35
broadli <929230157@qq.com>
2020-06-01 13:03:11 +0800

    查询分类列表之后一直失败问题

--- server/source/cpp/manager/db/db_base.cpp
+++ server/source/cpp/manager/db/db_base.cpp
@@ -476,34 +476,38 @@ Json::Value DBBase::selectCategory(string title, int uid, string &Msg)
 
        MYSQL_ROW m_row;
        MYSQL_RES *m_res;
-       if (mysql_field_count(&mysql) > 0)
-       {
-              LOGD("[db_base.querycategory] handle category db mysql_field_count > 0");
-              if (m_res = mysql_store_result(&mysql))
-              {
-                     while (m_row = mysql_fetch_row(m_res))
-                     {
-                            stringstream ss;
-                            ss << m_row[0];
-                            int i_id;
-                            ss >> i_id;
-                            if (i_id <= 0)
-                            {
-                                   LOGD("[db_base.querycategory] handle category db mysql_query empty , id = " + (string)m_row[0]);
-                                   break;
-                            }
-                            LOGD("[db_base.querycategory] handle category db get data");
 
-                            root["ID"] = m_row[0];
-                            root["TITLE"] = m_row[1];
-                            root["UID"] = m_row[2];
-                            root["is_empty"] = false;
-                     }
-              }
+       //获取查询结果
+       m_res = mysql_store_result(&mysql);
+       if (m_res == NULL)
+       {
+              Msg = "[db_base.selectUserAccountByAccount] select m_res null";
+              //释放读锁
+              rwlock->readUnlock();
               mysql_free_result(m_res);
+              return root;
        }
-       while (mysql_next_result(&mysql))
-              ;
+
+       //这里只会返回一条数据
+       while (m_row = mysql_fetch_row(m_res))
+       {
+              //小于0则表示查询无结果或失败
+              stringstream ss;
+              ss << m_row[0];
+              int i_id;
+              ss >> i_id;
+              if (i_id <= 0)
+              {
+                     LOGD("[db_base.selectUserAccountByAccount] handle account db mysql_query empty , id = " + (string)m_row[0]);
+                     break;
+              }
+              root["ID"] = m_row[0];
+              root["TITLE"] = m_row[1];
+              root["UID"] = m_row[2];
+              root["is_empty"] = false;
+       }
+       mysql_free_result(m_res);
+
        Json::FastWriter fw;
        LOGD("[db_base.selectCategory] root: " + fw.write(root));
        //释放读锁
