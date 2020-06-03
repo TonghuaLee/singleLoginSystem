1264153ac029d76dc2f70a867bd51a26415b7f6b
broadli <929230157@qq.com>
2020-06-03 23:12:19 +0800

    feat: 修改token解决密钥问题

--- client/singleloginapp/djinni/authso/utils/network_utils.cpp
+++ client/singleloginapp/djinni/authso/utils/network_utils.cpp
@@ -10,15 +10,15 @@ using namespace grpc;
 
 using grpc::Status;
 
-
-
 // TokenAuthenticator 用来支持令牌认证
 // https://grpc.io/docs/guides/auth.html
-class TokenAuthenticator : public grpc::MetadataCredentialsPlugin {
+class TokenAuthenticator : public grpc::MetadataCredentialsPlugin
+{
 public:
-    TokenAuthenticator(const std::string& token) : token_(token) {}
+    TokenAuthenticator(const std::string &token) : token_(token) {}
 
-    grpc::Status GetMetadata(grpc::string_ref service_url, grpc::string_ref method_name, const grpc::AuthContext& channel_auth_context, std::multimap<grpc::string, grpc::string>* metadata) override {
+    grpc::Status GetMetadata(grpc::string_ref service_url, grpc::string_ref method_name, const grpc::AuthContext &channel_auth_context, std::multimap<grpc::string, grpc::string> *metadata) override
+    {
         metadata->insert(std::make_pair("authorization-token", token_));
         return Status::OK;
     }
@@ -29,36 +29,38 @@ private:
 
 std::shared_ptr<grpc::Channel> utils::NetworkUtils::channel = NULL;
 
-std::shared_ptr<grpc::Channel> utils::NetworkUtils::getNetworkChannel(){
+std::shared_ptr<grpc::Channel> utils::NetworkUtils::getNetworkChannel()
+{
 
-    if (channel != NULL){
+    if (channel != NULL)
+    {
         return channel;
     }
 
     const std::string MY_PEM = "-----BEGIN CERTIFICATE-----\n"
-                               "MIIDoDCCAogCCQDCwTZmK/3aCzANBgkqhkiG9w0BAQUFADCBkTELMAkGA1UEBhMC\n"
-                               "Q04xETAPBgNVBAgMCFNoZW5aaGVuMREwDwYDVQQHDAhTaGVuWmhlbjEOMAwGA1UE\n"
-                               "CgwFaGFwcHkxDjAMBgNVBAsMBWhhcHB5MRswGQYDVQQDDBJ3d3cuaGFwcHlicm9h\n"
-                               "ZC5jb20xHzAdBgkqhkiG9w0BCQEWEDkyOTIzMDE1N0BxcS5jb20wHhcNMjAwNjAz\n"
-                               "MTAwNzQ5WhcNMzAwNjAxMTAwNzQ5WjCBkTELMAkGA1UEBhMCQ04xETAPBgNVBAgM\n"
-                               "CFNoZW5aaGVuMREwDwYDVQQHDAhTaGVuWmhlbjEOMAwGA1UECgwFaGFwcHkxDjAM\n"
-                               "BgNVBAsMBWhhcHB5MRswGQYDVQQDDBJ3d3cuaGFwcHlicm9hZC5jb20xHzAdBgkq\n"
-                               "hkiG9w0BCQEWEDkyOTIzMDE1N0BxcS5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IB\n"
-                               "DwAwggEKAoIBAQDLuJx6S20eeTd0q0wj+70Dpf+pSFzBTAghB2USk/t0INoenf+x\n"
-                               "+JdMqgPaIfkoUOd1IeMXU8jjwrvmE3jCg92DxV3V10yetn0nOv+hEDEoss319f+m\n"
-                               "jpCz6mzxGbb7rHNXDxVkdwxPMw7bNYeBRxktZBSHx/i4x4n1KO3y4kpQAt80kIhc\n"
-                               "7H2leRDDL/gMTqclBSGEG4Gm0jWNJnO8UXZvib2JR8xCXSTA5KJJL8t2dxPVtdC+\n"
-                               "apt5qcmLzlm08DK9pDvMfYdMD8dfpNTEZx7xeQGbKduw1RdV8ZJFhQ9qzunfRxlc\n"
-                               "PEmEfe6lbRgZgZ1fXo2k1Sw1at18aFnPevgHAgMBAAEwDQYJKoZIhvcNAQEFBQAD\n"
-                               "ggEBAGJL6OJUJdE5VoqZknlo3uWCjJV05FzHxzyTci6nVAO/w74Lg1OVCZe2WhK5\n"
-                               "NY2Pi9lQyBrT86F/QSVIUsQ4ApOYmLPLVPEtFxZaHIPJ4cbVQ9I55zb9MjwkJ3fk\n"
-                               "9UVLQt/t+LC/MacGuVhtu7Yh2kx2ll1uI5UlKxYoI9ncDqRT35VRR73HbzVYKkit\n"
-                               "WplHWGUI5+ZzbL0cJLk38Cd3hhdJhvWNNP4WWVqFR6LX0stdY4/HX569j8esTt0J\n"
-                               "K2hUdI63/3T3mL76bZ9siI81iSi2IJGAlWozg+zYl/XGvBB4lDYL4bj7SXXkgp5i\n"
-                               "vU8fijkjeO9d8l1NyHBs/37wTIY="
+                               "MIIDmTCCAoECCQDCwTZmK/3aDDANBgkqhkiG9w0BAQUFADCBjzELMAkGA1UEBhMC\n"
+                               "Q04xETAPBgNVBAgMCFNoZW5aaGVuMREwDwYDVQQHDAhTaGVuWmhlbjERMA8GA1UE\n"
+                               "CgwIU2hlblpoZW4xDjAMBgNVBAsMBWhhcHB5MRYwFAYDVQQDDA13d3cuaGFwcHku\n"
+                               "Y29tMR8wHQYJKoZIhvcNAQkBFhA5MjkyMzAxNTdAcXEuY29tMB4XDTIwMDYwMzE0\n"
+                               "MTQyOFoXDTMwMDYwMTE0MTQyOFowgYwxCzAJBgNVBAYTAkNOMREwDwYDVQQIDAhT\n"
+                               "aGVuWmhlbjERMA8GA1UEBwwIU2hlblpoZW4xDjAMBgNVBAoMBWhhcHB5MQ4wDAYD\n"
+                               "VQQLDAVoYXBweTEWMBQGA1UEAwwNd3d3LmhhcHB5LmNvbTEfMB0GCSqGSIb3DQEJ\n"
+                               "ARYQOTI5MjMwMTU3QHFxLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC\n"
+                               "ggEBAMu4nHpLbR55N3SrTCP7vQOl/6lIXMFMCCEHZRKT+3Qg2h6d/7H4l0yqA9oh\n"
+                               "+ShQ53Uh4xdTyOPCu+YTeMKD3YPFXdXXTJ62fSc6/6EQMSiyzfX1/6aOkLPqbPEZ\n"
+                               "tvusc1cPFWR3DE8zDts1h4FHGS1kFIfH+LjHifUo7fLiSlAC3zSQiFzsfaV5EMMv\n"
+                               "+AxOpyUFIYQbgabSNY0mc7xRdm+JvYlHzEJdJMDkokkvy3Z3E9W10L5qm3mpyYvO\n"
+                               "WbTwMr2kO8x9h0wPx1+k1MRnHvF5AZsp27DVF1XxkkWFD2rO6d9HGVw8SYR97qVt\n"
+                               "GBmBnV9ejaTVLDVq3XxoWc96+AcCAwEAATANBgkqhkiG9w0BAQUFAAOCAQEAT+Fb\n"
+                               "rUztdFXplnxr6dhHoe6gKOohg7jJDmq1ivP93xInR3M8BJS+CAOpMQ9w+Nufu5mt\n"
+                               "+KUKgPObZZYvW/35r6YWsCRkPCgMFLX0Oys7kW907wve0manypuusnQnXel/Tl8Z\n"
+                               "tUAys8r1JzIbSJOLbKiqCjVsyLUypapKV14rtQ/P5YUYsnGMX8xabw5W7pbkO6A1\n"
+                               "y4/Tqb4ldnBSql+FfgqZ0NK29nHLwt0ZKT7F8kU1Ip0/w+8QNEmLo2J+dUx7Uuy8\n"
+                               "SyWqHgwJnPy6q+e9IUqm2RdVjdL8ClnQmI6T0Yy/qA+X7GYMfep9732jXlKq0IWo\n"
+                               "QD5cD8LnEwkyZXmj5g==\n"
                                "-----END CERTIFICATE-----";
 
-    const std::string TOKEN = "happy-broad";
+    const std::string TOKEN = "www.happy.com";
 
     const std::string host("118.126.89.19");
     const int port = 50051;
@@ -66,7 +68,6 @@ std::shared_ptr<grpc::Channel> utils::NetworkUtils::getNetworkChannel(){
     const int host_port_buf_size = 1024;
     char host_port[host_port_buf_size];
     snprintf(host_port, host_port_buf_size, "%s:%d", host.c_str(), port);
-
 
     grpc::SslCredentialsOptions ssl_options;
     ssl_options.pem_root_certs = MY_PEM;
